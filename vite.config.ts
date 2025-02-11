fb5fb7712e64938e193a61a3133acbc45d6e186f
Alan Mendelevich <ailon@ailon.org>
2025-02-11 15:50:50 +0200

    add marker.js and svgr dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
+import svgr from "vite-plugin-svgr";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), svgr()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
