b9581f4260ddb530fc847a31fd5f67aaa30ad625
Alan Mendelevich <ailon@ailon.org>
2025-02-11 14:33:51 +0200

    add multi-page setup

---
+++ src/pages/frontpage/FrontPageApp.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-4xl">Front page</h1>
+    </div>
+  );
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "frontpage/index.html"),
+      },
     },
   },
 });
