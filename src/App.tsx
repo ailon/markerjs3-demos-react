183334313c8460ebbd1a22d285ba757660bd680b
Alan Mendelevich <ailon@ailon.org>
2025-02-11 14:01:23 +0200

    initial setup Vite + Tailwind 4 + shadcn

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1 className="text-4xl">Coming soon...</h1>
+    </div>
+  );
+}
+
+export default App;
