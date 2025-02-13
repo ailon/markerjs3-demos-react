94999cc619335d69b1e8fdf4b197eebb5b9870ef
Alan Mendelevich <ailon@ailon.org>
2025-02-13 14:04:57 +0200

    update layout of opacity panel

--- src/components/markerjs/toolbox/opacity-panel.tsx
+++ src/components/markerjs/toolbox/opacity-panel.tsx
@@ -5,6 +5,7 @@ import { OpacityIcon } from "../ui/icons";
 import { Slider } from "../../ui/slider";
 import { Input } from "../../ui/input";
 import ToolboxPanel from "../ui/toolbox-panel";
+import { Label } from "@/components/ui/label";
 
 type Props = {
   markerEditor: MarkerBaseEditor;
@@ -24,22 +25,28 @@ const OpacityPanel = ({ markerEditor }: Props) => {
 
   return (
     <ToolboxPanel title="Opacity" icon={OpacityIcon}>
-      <div className="flex items-center space-x-2">
+      <div className="flex flex-col space-y-4">
+        <div className="flex justify-between items-center space-x-2">
+          <Label htmlFor="opacityInput">Value:</Label>
+          <Input
+            id="opacityInput"
+            value={Math.round(opacity * 100)}
+            type="number"
+            min={0}
+            max={100}
+            step={10}
+            className="w-auto p-1 text-right"
+            onChange={(ev) =>
+              handleOpacityChange(ev.target.valueAsNumber / 100)
+            }
+          />
+        </div>
         <Slider
           value={[opacity]}
           min={0}
           max={1}
           step={0.1}
           onValueChange={(ev) => handleOpacityChange(ev[0])}
-        />
-        <Input
-          value={Math.round(opacity * 100)}
-          type="number"
-          min={0}
-          max={100}
-          step={10}
-          className="w-auto p-1 text-center"
-          onChange={(ev) => handleOpacityChange(ev.target.valueAsNumber / 100)}
         />
       </div>
     </ToolboxPanel>
