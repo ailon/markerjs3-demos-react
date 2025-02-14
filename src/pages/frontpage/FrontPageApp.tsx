ce4ab282be9decbc318728b4cf144b6d26e8d6c5
Alan Mendelevich <ailon@ailon.org>
2025-02-14 16:07:53 +0200

    add initial front page demo state

--- src/pages/frontpage/FrontPageApp.tsx
+++ src/pages/frontpage/FrontPageApp.tsx
@@ -1,9 +1,13 @@
 import Editor from "@/components/markerjs/editor";
+import demoState from "./frontpage-state.json";
 
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <Editor targetImageSrc="/sample-images/phone-modules.jpg" />
+      <Editor
+        targetImageSrc="/sample-images/phone-modules.jpg"
+        annotation={demoState}
+      />
     </div>
   );
 }
