5e3fd77c65aa73d7dbc139e033f3e189502bf83a
ailon <ailon@ailon.org>
2025-02-18 08:53:45 +0200

    change zoom icons

--- src/components/markerjs/ui/icons.tsx
+++ src/components/markerjs/ui/icons.tsx
@@ -5,9 +5,9 @@ import DeleteIcon from "@/assets/icons/trash.svg?react";
 import DownloadIcon from "@/assets/icons/download.svg?react";
 import UndoIcon from "@/assets/icons/arrow-back-up.svg?react";
 import RedoIcon from "@/assets/icons/arrow-forward-up.svg?react";
-import ZoomInIcon from "@/assets/icons/zoom-in.svg?react";
-import ZoomOutIcon from "@/assets/icons/zoom-out.svg?react";
-import ZoomResetIcon from "@/assets/icons/zoom-reset.svg?react";
+import ZoomInIcon from "@/assets/icons/plus.svg?react";
+import ZoomOutIcon from "@/assets/icons/minus.svg?react";
+import ZoomResetIcon from "@/assets/icons/relation-one-to-one.svg?react";
 
 import ChevronDownIcon from "@/assets/icons/chevron-down.svg?react";
 import AddIcon from "@/assets/icons/square-plus.svg?react";
